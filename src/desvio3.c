f3b8264fec8c2616b4906e655ee1b9f40f9ba089
David <dividindoconta@gmail.com>
2026-05-26 11:55:42 -0300

    desvio de fluxo usando o if... else..

--- src/desvio3.c
+++ src/desvio3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
@@ -6,6 +7,10 @@ float nota1;
 float nota2;
 float nota3;
 float nota4;
+
+// comando system que chama uma execuçao do sistema operacional
+//que neste caso sera o comando clear
+    system("clear");
 
     printf("digite a nota do aluno e tecle enter\n");
     scanf("%f",&nota1);
@@ -25,6 +30,14 @@ if (media >=7) {
 } else {
     printf("reprovado\n");
 }   
+
+    printf("\n##################################################\n");
+    printf("endereço de memoria varialvel nota1 %p \n", &nota1);
+    printf("endereço de memoria varialvel nota1 %p \n", &nota2);
+    printf("endereço de memoria varialvel nota1 %p \n", &nota3);
+    printf("endereço de memoria varialvel nota1 %p \n", &nota4);
+    printf("endereço de memoria varialvel nota1 %p \n", &media);
+
 
 return 0;
 
